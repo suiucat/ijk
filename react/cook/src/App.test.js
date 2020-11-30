c5ea54c486fe6caa3babae3b04c8682d412bed98
suiucat <suiusoldier@163.com>
2020-11-30 16:56:28 +0800

    react: add file

--- react/cook/src/App.test.js
+++ react/cook/src/App.test.js
@@ -1,8 +1,85 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ReactDOM from 'react-dom';
+import App, { Search, Table, Button} from './App';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('App', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  test('has a vaild snapshot', () => {
+    const component = renderer.create(
+      <App />
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+
+describe('Search', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search />, div);
+  });
+
+  test('has a vaild snapshot', () => {
+    const component = renderer.create(
+      <Search />
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+
+describe('Button', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button />, div);
+  });
+
+  test('has a vaild snapshot', () => {
+    const component = renderer.create(
+      <Button />
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Table', () => {
+  const props = {
+    list: [{ title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+           { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+          ],
+    };
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table { ...props }/>, div);
+  });
+
+  test('has a vaild snapshot', () => {
+    const component = renderer.create(
+      <Table { ...props }/>
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
--- react/cook/src/components/index.js
+++ react/cook/src/components/index.js
@@ -1,70 +1,9 @@
-const largeColumn = {
-  width: '40%'
-}
-
-const midColumn = {
-  width: '30%'
-}
-
-const samllColumn = {
-  width: '10%'
-}
-
-const Search = ({ value, onSubmit, onChange, children}) => {
-  return (
-    <form
-      className="search"
-      onSubmit={onSubmit}
-    >
-      <input
-        type="text"
-        value={value}
-        onChange={onChange}
-      />
-      <button
-        type="submit"
-      >
-        {children}
-      </button>
-    </form>
-  );
-}
-
-const Table = ({ list, onDismiss }) => {
-  return (
-    <div className="tabe">
-      {list.map(item => {
-        return (
-          <div key={item.objectID} className="table-row">
-            <span style={largeColumn}>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span style={midColumn}>
-              {item.author}
-            </span>
-            <span  style={samllColumn}>
-              {item.num_comments}
-            </span>
-            <span style={samllColumn}>
-              {item.points}
-            </span>
-            <span style={samllColumn}>
-              <button
-                onClick={() => onDismiss(item.objectID)}
-                type="button"
-                className="button-inline"
-              >
-                点击
-              </button>
-            </span>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
+import Button from './Button/index';
+import Table from './Table/index';
+import Search from './Search/index';
 
 export {
-  Search,
-  Table
+  Button,
+  Table,
+  Search 
 }
